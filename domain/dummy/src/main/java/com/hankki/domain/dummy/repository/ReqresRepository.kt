4bafe1ad95d850356861967e7aabcbc3c81581f4
Dongmin <pdm001125@naver.com>
2024-06-27 04:22:26 +0900

    Feature #3: init dummy Domain

---
+++ domain/dummy/src/main/java/com/hankki/domain/dummy/repository/ReqresRepository.kt
@@ -0,0 +1,7 @@
+package com.hankki.domain.dummy.repository
+
+import com.hankki.domain.dummy.entity.response.ReqresUserModel
+
+interface ReqresRepository {
+    suspend fun getUserList(page: Int): List<ReqresUserModel>
+}
