914ab93a5cfd2cc1a1543d26dff30413e3510050
Dongmin <pdm001125@naver.com>
2024-06-29 07:58:45 +0900

    fix #3: error package name

--- feature/home/src/androidTest/java/com/hankki/feature/home/ExampleInstrumentedTest.kt
+++ feature/home/src/androidTest/java/com/hankki/feature/home/ExampleInstrumentedTest.kt
@@ -2,7 +2,6 @@ package com.hankki.feature.home
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -19,6 +18,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.eoyeongbooyeong.feature.home", appContext.packageName)
+        assertEquals("com.hankki.feature.home", appContext.packageName)
     }
 }
