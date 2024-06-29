3339caf941b2df67706e655831de0ba4748453c8
Dongmin <pdm001125@naver.com>
2024-06-30 02:55:25 +0900

    Feature #6: connect Naver Map

--- feature/home/src/main/java/com/hankki/feature/home/HomeRoute.kt
+++ feature/home/src/main/java/com/hankki/feature/home/HomeRoute.kt
@@ -1,23 +1,29 @@
 package com.hankki.feature.home
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import com.naver.maps.map.compose.MapUiSettings
+import com.naver.maps.map.compose.NaverMap
 
 @Composable
 fun HomeRoute(paddingValues: PaddingValues) {
     HomeScreen(paddingValues)
 }
 
+@OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun HomeScreen(paddingValues: PaddingValues) {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(paddingValues)) {
-        Text(text = "Home")
-    }
+    NaverMap(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        uiSettings = MapUiSettings(
+            isZoomControlEnabled = false,
+            isScaleBarEnabled = false
+        )
+    )
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven{
+            url = uri("https://repository.map.naver.com/archive/maven")
+        }
     }
 }
 
