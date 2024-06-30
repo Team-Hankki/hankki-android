f882334736348a567db27ac848f2f84f34c8604f
Dongmin <pdm001125@naver.com>
2024-07-01 00:11:53 +0900

    Feature #8: rename HankkiTypography items

--- core/designsystem/src/main/java/com/hankki/core/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/hankki/core/designsystem/theme/Type.kt
@@ -21,26 +21,26 @@ val PretendardMedium = FontFamily(Font(R.font.pretendard_medium, FontWeight.Medi
 class HankkiTypography internal constructor(
     h1: TextStyle,
     h2: TextStyle,
-    suit_h1: TextStyle,
+    suitH1: TextStyle,
     sub1: TextStyle,
     sub2: TextStyle,
     sub3: TextStyle,
-    suit_s1: TextStyle,
+    suitS1: TextStyle,
     body1: TextStyle,
     body2: TextStyle,
     body3: TextStyle,
     body4: TextStyle,
-    suit_body1: TextStyle,
-    suit_body2: TextStyle,
+    suitBody1: TextStyle,
+    suitBody2: TextStyle,
     button1: TextStyle,
     caption1: TextStyle,
-    catption2: TextStyle,
+    caption2: TextStyle,
 ) {
     var h1: TextStyle by mutableStateOf(h1)
         private set
     var h2: TextStyle by mutableStateOf(h2)
         private set
-    var suit_h1: TextStyle by mutableStateOf(suit_h1)
+    var suitH1: TextStyle by mutableStateOf(suitH1)
         private set
     var sub1: TextStyle by mutableStateOf(sub1)
         private set
@@ -48,7 +48,7 @@ class HankkiTypography internal constructor(
         private set
     var sub3: TextStyle by mutableStateOf(sub3)
         private set
-    var suit_s1: TextStyle by mutableStateOf(suit_s1)
+    var suitS1: TextStyle by mutableStateOf(suitS1)
         private set
     var body1: TextStyle by mutableStateOf(body1)
         private set
@@ -58,48 +58,48 @@ class HankkiTypography internal constructor(
         private set
     var body4: TextStyle by mutableStateOf(body4)
         private set
-    var suit_body1: TextStyle by mutableStateOf(suit_body1)
+    var suitBody1: TextStyle by mutableStateOf(suitBody1)
         private set
-    var suit_body2: TextStyle by mutableStateOf(suit_body2)
+    var suitBody2: TextStyle by mutableStateOf(suitBody2)
         private set
     var button1: TextStyle by mutableStateOf(button1)
         private set
     var caption1: TextStyle by mutableStateOf(caption1)
         private set
-    var caption2: TextStyle by mutableStateOf(catption2)
+    var caption2: TextStyle by mutableStateOf(caption2)
         private set
 
     fun copy(
         h1: TextStyle = this.h1,
         h2: TextStyle = this.h2,
-        suit_h1: TextStyle = this.suit_h1,
+        suitH1: TextStyle = this.suitH1,
         sub1: TextStyle = this.sub1,
         sub2: TextStyle = this.sub2,
         sub3: TextStyle = this.sub3,
-        suit_s1: TextStyle = this.suit_s1,
+        suitS1: TextStyle = this.suitS1,
         body1: TextStyle = this.body1,
         body2: TextStyle = this.body2,
         body3: TextStyle = this.body3,
         body4: TextStyle = this.body4,
-        suit_body1: TextStyle = this.suit_body1,
-        suit_body2: TextStyle = this.suit_body2,
+        suitBody1: TextStyle = this.suitBody1,
+        suitBody2: TextStyle = this.suitBody2,
         button1: TextStyle = this.button1,
         caption1: TextStyle = this.caption1,
         caption2: TextStyle = this.caption2,
     ): HankkiTypography = HankkiTypography(
         h1,
         h2,
-        suit_h1,
+        suitH1,
         sub1,
         sub2,
         sub3,
-        suit_s1,
+        suitS1,
         body1,
         body2,
         body3,
         body4,
-        suit_body1,
-        suit_body2,
+        suitBody1,
+        suitBody2,
         button1,
         caption1,
         caption2
@@ -108,17 +108,17 @@ class HankkiTypography internal constructor(
     fun update(other: HankkiTypography) {
         h1 = other.h1
         h2 = other.h2
-        suit_h1 = other.suit_h1
+        suitH1 = other.suitH1
         sub1 = other.sub1
         sub2 = other.sub2
         sub3 = other.sub3
-        suit_s1 = other.suit_s1
+        suitS1 = other.suitS1
         body1 = other.body1
         body2 = other.body2
         body3 = other.body3
         body4 = other.body4
-        suit_body1 = other.suit_body1
-        suit_body2 = other.suit_body2
+        suitBody1 = other.suitBody1
+        suitBody2 = other.suitBody2
         button1 = other.button1
         caption1 = other.caption1
         caption2 = other.caption2
@@ -138,7 +138,7 @@ fun hankkiTypography(): HankkiTypography {
             fontWeight = FontWeight.Bold,
             fontSize = 24.sp
         ),
-        suit_h1 = TextStyle(
+        suitH1 = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp
@@ -158,7 +158,7 @@ fun hankkiTypography(): HankkiTypography {
             fontWeight = FontWeight.Bold,
             fontSize = 12.sp
         ),
-        suit_s1 = TextStyle(
+        suitS1 = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Bold,
             fontSize = 10.sp
@@ -184,12 +184,12 @@ fun hankkiTypography(): HankkiTypography {
             fontWeight = FontWeight.Normal,
             fontSize = 10.sp
         ),
-        suit_body1 = TextStyle(
+        suitBody1 = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
             fontSize = 8.sp
         ),
-        suit_body2 = TextStyle(
+        suitBody2 = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
             fontSize = 6.sp
@@ -204,7 +204,7 @@ fun hankkiTypography(): HankkiTypography {
             fontWeight = FontWeight.Normal,
             fontSize = 12.sp
         ),
-        catption2 = TextStyle(
+        caption2 = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
             fontSize = 10.sp
