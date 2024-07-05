6b32c7f6a7f7e0fb399093bca1105d02691fa018
0se0 <030ggong@gmail.com>
2024-07-05 22:12:31 +0900

    Feature #20: Separate UniversityItem component

---
+++ feature/universityselection/src/main/java/com/hankki/feature/universityselection/component/UniversityItem.kt
@@ -0,0 +1,31 @@
+package com.hankki.feature.universityselection
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.hankki.core.common.extension.noRippleClickable
+import com.hankki.core.designsystem.theme.HankkiTheme
+import com.hankki.core.designsystem.theme.Red
+import com.hankki.domain.universityselection.UniversitySelectionModel
+
+@Composable
+fun UniversityItem(
+    university: UniversitySelectionModel,
+    isSelected: Boolean,
+    onSelectUniversity: (String) -> Unit
+) {
+    Text(
+        text = university.name,
+        style = HankkiTheme.typography.body1,
+        modifier = Modifier
+            .fillMaxWidth()
+            .noRippleClickable {
+                onSelectUniversity(university.name)
+            }
+            .padding(14.dp),
+        color = if (isSelected) Red else Gray800
+    )
+}
