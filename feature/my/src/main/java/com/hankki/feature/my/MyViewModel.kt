6422f84a0d0edab604d3f620e60686909c1474eb
hyeumm <20221159@sungshin.ac.kr>
2024-07-06 04:13:38 +0900

    Add #18: MyViewModel

---
+++ feature/my/src/main/java/com/hankki/feature/my/MyViewModel.kt
@@ -0,0 +1,29 @@
+package com.hankki.feature.my
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hankki.domain.my.entity.UserInfoEntity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MyViewModel @Inject constructor() : ViewModel() {
+    private val _myState = MutableStateFlow(MyState())
+    val myState: StateFlow<MyState>
+        get() = _myState.asStateFlow()
+
+    fun loadMockInformation() {
+        viewModelScope.launch {
+            val mockUserName = "송한끼"
+            val mockUserImage = R.drawable.ic_good
+            val mockUserInformation = UserInfoEntity(mockUserName, mockUserImage)
+            _myState.value = _myState.value.copy(
+                userState = mockUserInformation
+            )
+        }
+    }
+}
