8a0be751771b65f89c2ba5890be07a6a62e39b6a
hyeumm <20221159@sungshin.ac.kr>
2024-07-07 01:33:10 +0900

    Add #18: add text align center

--- feature/my/src/main/java/com/hankki/feature/my/component/ButtonWithImageAndBorder.kt
+++ feature/my/src/main/java/com/hankki/feature/my/component/ButtonWithImageAndBorder.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hankki.core.designsystem.theme.Gray200
@@ -52,7 +53,8 @@ fun ButtonWithImageAndBorder(
         Text(
             text = buttonDescription,
             style = HankkiTheme.typography.body3,
-            color = Gray900
+            color = Gray900,
+            textAlign = TextAlign.Center
         )
     }
 }
