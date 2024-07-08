0a2166465220ec33eb53b25169f66c55428980db
Dongmin <pdm001125@naver.com>
2024-07-08 15:09:49 +0900

    Chore #16: chore color order

--- core/designsystem/src/main/java/com/hankki/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/hankki/core/designsystem/theme/Color.kt
@@ -13,8 +13,8 @@ val Pink40 = Color(0xFF7D5260)
 //main color
 val Red = Color(0xFFFF4C24)
 val RedLight = Color(0xFFFFF5F3)
-val YellowLight = Color(0xFFFFFFEB)
 val Yellow = Color(0xFFF8F891)
+val YellowLight = Color(0xFFFFFFEB)
 
 //gray scale
 val White = Color(0xFFFFFFFF)
