3acaf3eee2ab89e106d64e34cec8ef35da7dfb1b
0se0 <030ggong@gmail.com>
2024-07-09 17:13:59 +0900

    Feature #17: BaseResponse

---
+++ data/login/src/main/java/com/hankki/data/login/dto/BaseResponse.kt
@@ -0,0 +1,14 @@
+package com.hankki.data.login.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BaseResponse<T>(
+    @SerialName("status")
+    val status: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: T,
+)
