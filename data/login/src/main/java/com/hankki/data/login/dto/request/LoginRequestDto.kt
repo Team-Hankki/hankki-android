f83b8dde665e54032799aaed87501839c18c2346
0se0 <030ggong@gmail.com>
2024-07-09 17:13:24 +0900

    Feature #17: LoginRequestDto

---
+++ data/login/src/main/java/com/hankki/data/login/dto/request/LoginRequestDto.kt
@@ -0,0 +1,14 @@
+package com.hankki.data.login.dto.request
+
+import com.hankki.domain.login.entity.request.LoginRequestModel
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginRequestDto (
+    @SerialName("platform")
+    val platform: String
+)
+
+fun LoginRequestModel.toLoginRequestDto(): LoginRequestDto =
+    LoginRequestDto(platform)
