05be2dd667ca1e6193723869df0f755d2e489ad9
0se0 <030ggong@gmail.com>
2024-07-09 23:51:44 +0900

    Mod #17: Return an instance directly

--- data/login/src/main/java/com/hankki/data/login/dto/response/LoginResponseDto.kt
+++ data/login/src/main/java/com/hankki/data/login/dto/response/LoginResponseDto.kt
@@ -2,8 +2,10 @@ package com.hankki.data.login.dto.response
 
 import com.hankki.domain.login.entity.response.LoginResponseModel
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-data class LoginResponseDto (
+@Serializable
+data class LoginResponseDto(
     @SerialName("accessToken")
     val accessToken: String,
     @SerialName("refreshToken")
@@ -12,5 +14,9 @@ data class LoginResponseDto (
     val isRegistered: Boolean
 ) {
     fun toLoginModel() =
-        LoginResponseModel(accessToken, refreshToken, isRegistered)
+        LoginResponseModel(
+            accessToken = accessToken,
+            refreshToken = refreshToken,
+            isRegistered = isRegistered
+        )
 }
