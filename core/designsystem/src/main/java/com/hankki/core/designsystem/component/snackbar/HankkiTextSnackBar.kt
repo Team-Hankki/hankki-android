bd289e1fbf0218e5aa3084a3d0b032c124a71dbb
0se0 <030ggong@gmail.com>
2024-07-10 13:40:13 +0900

    Feature #34: HankkiTextSnackBar

---
+++ core/designsystem/src/main/java/com/hankki/core/designsystem/component/snackbar/HankkiTextSnackBar.kt
@@ -0,0 +1,44 @@
+package com.hankki.core.designsystem.component.snackbar
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hankki.core.designsystem.theme.HankkiTheme
+
+@Composable
+fun HankkiTextSnackBar(message: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        HankkiSnackBar {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp, vertical = 4.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = message,
+                    style = HankkiTheme.typography.body3,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHankkiTextSnackBar() {
+    HankkiTextSnackBar(message = "나의 족보에 추가되었습니다.")
+}
