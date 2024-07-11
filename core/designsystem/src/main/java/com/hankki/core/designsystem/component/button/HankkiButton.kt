0a38509fa5c24e26e3dd490d8fa8a85cf2c5524d
Dongmin <pdm001125@naver.com>
2024-07-11 23:48:48 +0900

    Feature #43: feat HankkiTextButton and bounceClick

--- core/designsystem/src/main/java/com/hankki/core/designsystem/component/button/HankkiButton.kt
+++ core/designsystem/src/main/java/com/hankki/core/designsystem/component/button/HankkiButton.kt
@@ -2,6 +2,7 @@ package com.hankki.core.designsystem.component.button
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -9,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,7 +26,7 @@ fun HankkiButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    textStyle: TextStyle = TextStyle.Default
+    textStyle: TextStyle = TextStyle.Default,
 ) {
     Box(
         contentAlignment = Alignment.Center,
@@ -35,7 +37,7 @@ fun HankkiButton(
                 blackAlpha = 0.16f,
                 onClick = onClick
             )
-            .padding(vertical = 16.dp, horizontal = 22.dp)
+            .padding(vertical = 15.dp, horizontal = 22.dp)
     ) {
         Text(
             text = text,
@@ -45,10 +47,41 @@ fun HankkiButton(
     }
 }
 
+@Composable
+fun HankkiTextButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    textStyle: TextStyle = TextStyle.Default,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color.Transparent)
+            .bounceClick(
+                blackAlpha = 0.16f,
+                onClick = onClick
+            )
+            .padding(vertical = 15.dp, horizontal = 22.dp)
+    ) {
+        Text(
+            text = text,
+            style = textStyle,
+            color = Red
+        )
+    }
+}
+
 @Preview
 @Composable
 fun HankkiButtonPreview() {
     HankkijogboTheme {
-        HankkiButton(text = "로그아웃", onClick = {})
+        Column {
+            HankkiButton(text = "로그아웃", onClick = {})
+            HankkiTextButton(text = "돌아가기", onClick = {})
+        }
+
     }
 }
