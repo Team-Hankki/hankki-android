69bfe2e54d64525f5eb09a7cd95892483643aedc
0se0 <030ggong@gmail.com>
2024-07-12 07:19:49 +0900

    Chore #36: data login module

---
+++ data/login/src/main/java/com/hankki/data/login/di/LoginRepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.hankki.data.login.di
+
+import com.hankki.data.login.repositoryImpl.LoginRepositoryImpl
+import com.hankki.domain.login.repository.LoginRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class LoginRepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun provideLoginRepository(
+        loginRepositoryImpl: LoginRepositoryImpl
+    ): LoginRepository
+}
---
+++ data/login/src/main/java/com/hankki/data/login/di/ServiceModule.kt
@@ -0,0 +1,21 @@
+package com.hankki.data.login.di
+
+import com.hankki.core.network.JWT
+import com.hankki.core.network.Reissue
+import com.hankki.data.login.service.LoginService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+
+    @Provides
+    @Singleton
+    fun provideLoginService(@JWT retrofit: Retrofit): LoginService =
+        retrofit.create(LoginService::class.java)
+}
