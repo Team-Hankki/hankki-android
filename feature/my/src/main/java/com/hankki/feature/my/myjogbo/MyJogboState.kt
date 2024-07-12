8e7c9a52fc4cb9d393167e18bf232b2b19d090a6
hyeumm <20221159@sungshin.ac.kr>
2024-07-12 18:30:43 +0900

    Add #26: add viewmodel function

--- feature/my/src/main/java/com/hankki/feature/my/myjogbo/MyJogboState.kt
+++ feature/my/src/main/java/com/hankki/feature/my/myjogbo/MyJogboState.kt
@@ -1,9 +1,12 @@
 package com.hankki.feature.my.myjogbo
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import com.hankki.feature.my.myjogbo.model.MyJogboModel
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 
 data class MyJogboState(
+    val editMode : MutableState<Boolean> = mutableStateOf(false),
     val myJogboItems: PersistentList<MyJogboModel> = persistentListOf()
 )
--- feature/my/src/main/java/com/hankki/feature/my/myjogbo/MyJogboViewModel.kt
+++ feature/my/src/main/java/com/hankki/feature/my/myjogbo/MyJogboViewModel.kt
@@ -1,5 +1,6 @@
 package com.hankki.feature.my.myjogbo
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.hankki.domain.my.entity.MyJogboEntity
 import com.hankki.feature.my.myjogbo.model.toMyJogboModel
@@ -32,6 +33,26 @@ class MyJogboViewModel @Inject constructor(
                     3,
                     jogboName = "족보이름3",
                 ).toMyJogboModel()
+            )
+        )
+    }
+
+    fun updateMode() {
+        _myJogboState.value = _myJogboState.value.copy(
+            editMode = mutableStateOf(
+                !_myJogboState.value.editMode.value
+            )
+        )
+    }
+
+    fun updateJogboSeleted(index: Int, isJogboSelected: Boolean) {
+        if (_myJogboState.value.myJogboItems.size <= index) return
+        if (_myJogboState.value.myJogboItems.isEmpty()) return
+
+        _myJogboState.value = _myJogboState.value.copy(
+            myJogboItems = _myJogboState.value.myJogboItems.set(
+                index,
+                _myJogboState.value.myJogboItems[index].copy(jogboSelected = isJogboSelected)
             )
         )
     }
