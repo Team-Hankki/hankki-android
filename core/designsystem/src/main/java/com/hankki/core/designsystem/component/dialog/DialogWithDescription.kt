8ca9aabfb54aa96b60750caab05ed3e78634675d
hyeumm <20221159@sungshin.ac.kr>
2024-07-14 23:44:55 +0900

    Feature #26: DialogWithDescription

---
+++ core/designsystem/src/main/java/com/hankki/core/designsystem/component/dialog/DialogWithDescription.kt
@@ -0,0 +1,84 @@
+package com.hankki.core.designsystem.component.dialog
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.hankki.core.designsystem.component.button.HankkiButton
+import com.hankki.core.designsystem.theme.Gray500
+import com.hankki.core.designsystem.theme.Gray900
+import com.hankki.core.designsystem.theme.HankkiTheme
+import com.hankki.core.designsystem.theme.HankkijogboTheme
+import com.hankki.core.designsystem.theme.White
+
+@Composable
+fun DialogWithDescription(
+    title: String,
+    description: String,
+    buttonTitle: String,
+    onConfirmation: () -> Unit
+) {
+    Dialog(onDismissRequest = { onConfirmation() }) {
+        Card(
+            shape = RoundedCornerShape(24.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = White
+            ),
+        ) {
+            Column(
+                modifier = Modifier.padding(20.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.End
+            ) {
+                Text(
+                    text = title,
+                    style = HankkiTheme.typography.sub1,
+                    color = Gray900,
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+
+                Text(
+                    text = description,
+                    style = HankkiTheme.typography.body4,
+                    color = Gray500,
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                HankkiButton(
+                    text = buttonTitle,
+                    onClick = { onConfirmation() },
+                    enabled = true,
+                    textStyle = HankkiTheme.typography.sub3
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun DialogWithDeleteButtonPrev() {
+    HankkijogboTheme {
+        DialogWithDescription("등록된 식당이 있어요\n식당으로 이동할까요?", "친구에게 내 족보를 공유할 수 있도록\n준비 중이에요", "확인", {})
+    }
+}
