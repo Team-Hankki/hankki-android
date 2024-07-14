6d133583eb0c5601cddd2cae859fea7c69801cd4
hyeumm <20221159@sungshin.ac.kr>
2024-07-14 23:35:52 +0900

    Mod #26: JogboItem

--- feature/my/src/main/java/com/hankki/feature/my/component/JogboItem.kt
+++ feature/my/src/main/java/com/hankki/feature/my/component/JogboItem.kt
@@ -3,7 +3,6 @@ package com.hankki.feature.my.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,64 +11,75 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.hankki.core.designsystem.theme.Gray850
+import coil.compose.AsyncImage
+import com.hankki.core.common.extension.noRippleClickable
+import com.hankki.core.designsystem.theme.Gray100
+import com.hankki.core.designsystem.theme.Gray800
 import com.hankki.core.designsystem.theme.HankkijogboTheme
 import com.hankki.core.designsystem.theme.Red
-import com.hankki.core.designsystem.theme.White
 import com.hankki.feature.my.R
 
 @Composable
 fun JogboItem(
+    title: String,
+    image: String,
     modifier: Modifier = Modifier,
-    isEditMode: MutableState<Boolean> = mutableStateOf(false),
-    jogboTitle: String = "",
-    isJogboSelected: Boolean = false,
-    editJogbo: () -> Unit = {},
-    moveToJogbo: () -> Unit = {}
+    isEditMode: Boolean = false,
+    isSelected: Boolean = false,
+    editJogbo: () -> Unit,
+    navigateToJogboDetail: () -> Unit
 ) {
     Box(
-        modifier = modifier
+        modifier = if (!isEditMode) modifier
             .clip(RoundedCornerShape(12.dp))
-            .background(Gray850)
+            .background(Gray100)
+            .noRippleClickable(onClick = navigateToJogboDetail)
+        else modifier
+            .clip(RoundedCornerShape(12.dp))
+            .background(Gray100)
+            .noRippleClickable(onClick = editJogbo)
     ) {
         Column(
-            modifier = if (!isEditMode.value) Modifier
-                .wrapContentSize()
-                .clickable(onClick = moveToJogbo)
-            else Modifier
-                .wrapContentSize()
-                .clickable(onClick = editJogbo),
+            modifier = Modifier
+                .wrapContentSize(),
 
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             Box {
+                Column {
+                    JogboItemText(
+                        text = "\n\n",
+                        color = Color.Transparent
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_add_jogbo_disable),
+                        contentDescription = null,
+                        modifier = Modifier.alpha(0f)
+                    )
+                }
                 JogboItemText(
-                    text = "\n\n",
-                    color = Color.Transparent
-                )
-                JogboItemText(
-                    text = jogboTitle,
-                    color = White
+                    text = title,
+                    color = Gray800
                 )
             }
-            Image(
-                painter = painterResource(id = R.drawable.ic_my_jogbo),
+            AsyncImage(
+                model = image,
                 contentDescription = null,
                 modifier = modifier.fillMaxWidth()
             )
         }
-        if (isJogboSelected) {
+        if (isSelected) {
             Box(
                 modifier = Modifier
                     .matchParentSize()
@@ -78,7 +88,6 @@ fun JogboItem(
                         color = Red,
                         shape = RoundedCornerShape(12.dp)
                     )
-                    .background(Red.copy(alpha = 0.4f))
             ) {
                 Image(
                     painter = painterResource(id = com.hankki.core.designsystem.R.drawable.ic_check_btn),
@@ -103,11 +112,11 @@ fun ItemPreview() {
 
     HankkijogboTheme {
         JogboItem(
-            jogboTitle = "새로운 족보 리스트 추가하기",
-            isJogboSelected = isSelected.value,
-            moveToJogbo = {
-                isSelected.value = !isSelected.value
-            }
+            title = "새로운 족보 리스트 추가하기",
+            image = "",
+            isSelected = isSelected.value,
+            editJogbo = {},
+            navigateToJogboDetail = {}
         )
     }
 }
