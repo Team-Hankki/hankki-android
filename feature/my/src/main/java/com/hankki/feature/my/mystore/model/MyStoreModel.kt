990ebca9a07e096b55d1ca935c4ae0dc0ef420c7
hyeumm <20221159@sungshin.ac.kr>
2024-07-15 19:24:36 +0900

    Add #26: add default value

--- feature/my/src/main/java/com/hankki/feature/my/mystore/model/MyStoreModel.kt
+++ feature/my/src/main/java/com/hankki/feature/my/mystore/model/MyStoreModel.kt
@@ -3,13 +3,13 @@ package com.hankki.feature.my.mystore.model
 import com.hankki.domain.my.entity.StoreEntity
 
 data class MyStoreModel(
-    val category: String,
-    val heartCount: Int,
-    val id: Int,
-    val imageURL: String,
+    val category: String = "",
+    val heartCount: Int = 0,
+    val id: Int = 0,
+    val imageURL: String = "",
     val isLiked: Boolean? = null,
-    val lowestPrice: Int,
-    val name: String
+    val lowestPrice: Int = 0,
+    val name: String = ""
 )
 
 
