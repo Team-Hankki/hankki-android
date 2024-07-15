4216f9f197a961ee98407592137ace8233898db1
0se0 <030ggong@gmail.com>
2024-07-16 00:20:53 +0900

    Mod #49: State Management

--- feature/storedetail/src/main/java/com/hankki/feature/storedetail/StoreDetailViewModel.kt
+++ feature/storedetail/src/main/java/com/hankki/feature/storedetail/StoreDetailViewModel.kt
@@ -1,7 +1,6 @@
-package com.hankki.feature.storedetail
-
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hankki.core.common.utill.UiState
 import com.hankki.feature.storedetail.model.MenuItem
 import com.hankki.feature.storedetail.model.StoreDetail
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,44 +9,58 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class StoreDetailViewModel : ViewModel() {
-    private val _storeDetail = MutableStateFlow<StoreDetail?>(null)
-    val storeDetail: StateFlow<StoreDetail?> = _storeDetail.asStateFlow()
+    private val _storeDetail = MutableStateFlow<UiState<StoreDetail>>(UiState.Loading)
+    val storeDetail: StateFlow<UiState<StoreDetail>> get() = _storeDetail.asStateFlow()
 
-    private var mockStoreDetail = StoreDetail(
-        name = "한끼네 한정식",
-        category = "한식",
-        isLiked = true,
-        heartCount = 300,
-        imageUrls = listOf(
-            "www.example.com/image1",
-            "www.example.com/image2",
-            "www.example.com/image3"
-        ),
-        menus = listOf(
+    private val _isLiked = MutableStateFlow(false)
+    val isLiked: StateFlow<Boolean> get() = _isLiked.asStateFlow()
+
+    private val _heartCount = MutableStateFlow(0)
+    val heartCount: StateFlow<Int> get() = _heartCount.asStateFlow()
+
+    private val _selectedIndex = MutableStateFlow(-1)
+    val selectedIndex: StateFlow<Int> get() = _selectedIndex.asStateFlow()
+
+    private val _buttonLabels = MutableStateFlow(listOf<String>())
+    val buttonLabels: StateFlow<List<String>> get() = _buttonLabels.asStateFlow()
+
+    init {
+        val mockMenus = listOf(
             MenuItem(name = "수육정식", price = 7900),
             MenuItem(name = "제육정식", price = 8900),
             MenuItem(name = "꼬막정식", price = 7900)
         )
-    )
+        val mockStoreDetail = StoreDetail(
+            name = "한끼네 한정식",
+            category = "한식",
+            isLiked = false,
+            heartCount = 299,
+            imageUrls = listOf(),
+            menus = mockMenus
+        )
 
-    init {
-        fetchStoreDetail()
+        setStoreDetail(mockStoreDetail)
+        _buttonLabels.value = listOf(
+            "식당이 사라졌어요",
+            "더이상 8000원이하인 메뉴가 없어요",
+            "부적절한 제보에요"
+        )
     }
 
-    private fun fetchStoreDetail() {
-        viewModelScope.launch {
-            _storeDetail.value = mockStoreDetail
-        }
+    private fun setStoreDetail(storeDetail: StoreDetail) {
+        _storeDetail.value = UiState.Success(storeDetail)
+        _isLiked.value = storeDetail.isLiked
+        _heartCount.value = storeDetail.heartCount
     }
 
     fun toggleLike() {
         viewModelScope.launch {
-            val newHeartCount =
-                if (mockStoreDetail.isLiked) mockStoreDetail.heartCount - 1 else mockStoreDetail.heartCount + 1
-            mockStoreDetail =
-                mockStoreDetail.copy(isLiked = !mockStoreDetail.isLiked, heartCount = newHeartCount)
-
-            _storeDetail.value = mockStoreDetail
+            _isLiked.value = !_isLiked.value
+            _heartCount.value = if (_isLiked.value) _heartCount.value + 1 else _heartCount.value - 1
         }
+    }
+
+    fun updateSelectedIndex(index: Int) {
+        _selectedIndex.value = index
     }
 }
