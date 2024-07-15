761bdc0c4ecabbc0a0ec770af606d24a37109221
Dongmin <pdm001125@naver.com>
2024-07-15 22:03:53 +0900

    Feature #59: feat setting status bar & navigation bar color

--- app/src/main/java/com/hankki/hankkijogbo/App.kt
+++ app/src/main/java/com/hankki/hankkijogbo/App.kt
@@ -12,17 +12,11 @@ class App : Application() {
         super.onCreate()
 
         initTimber()
-        setDarkMode()
         initKakaoSdk()
     }
 
     private fun initTimber() {
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
-    }
-
-    private fun setDarkMode() {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) // status bar color를 위해서 설정
-        // statusbar color를 바꾸는 속성을 찾으면 수정 예정
     }
 
     private fun initKakaoSdk() {
--- core/designsystem/src/main/java/com/hankki/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/hankki/core/designsystem/theme/Theme.kt
@@ -1,34 +1,26 @@
 package com.hankki.core.designsystem.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val DarkColorScheme = lightColorScheme(
+    primary = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = White
 )
 
 private val LocalHankkiTypography = staticCompositionLocalOf<HankkiTypography> {
@@ -62,8 +54,27 @@ fun HankkijogboTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = LightColorScheme // 현재는 다크테마 미지원. 추후 지원시 수정 예정
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
+    }
     val typography = hankkiTypography()
+
+    // set status bar & navigation bar color
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            window.navigationBarColor = White.toArgb()
+
+            WindowCompat.getInsetsController(window, view)
+                .isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view)
+                .isAppearanceLightNavigationBars = darkTheme
+        }
+    }
 
     ProvideHankkiTypography(typography) {
         MaterialTheme(
