9e3a882079eb205e5273d79a3af9655e6c2dad51
Dongmin <pdm001125@naver.com>
2024-07-16 19:52:47 +0900

    Feature #66: feat report data

--- data/report/src/main/java/com/hankki/data/report/datasourceimpl/ReportDataSourceImpl.kt
+++ data/report/src/main/java/com/hankki/data/report/datasourceimpl/ReportDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.hankki.data.report.datasourceimpl
 
 import com.hankki.core.network.BaseResponse
 import com.hankki.data.report.datasource.ReportDataSource
+import com.hankki.data.report.dto.response.CountResponseDto
 import com.hankki.data.report.dto.response.LocationsResponseDto
 import com.hankki.data.report.service.ReportService
 import javax.inject.Inject
@@ -9,5 +10,9 @@ import javax.inject.Inject
 class ReportDataSourceImpl @Inject constructor(
     private val reportService: ReportService,
 ) : ReportDataSource {
-    override suspend fun getLocations(query: String): BaseResponse<LocationsResponseDto> = reportService.getStoreLocation(query)
+    override suspend fun getLocations(query: String): BaseResponse<LocationsResponseDto> =
+        reportService.getStoreLocation(query)
+
+    override suspend fun getReportsCount(): BaseResponse<CountResponseDto> =
+        reportService.getReportsCount()
 }
