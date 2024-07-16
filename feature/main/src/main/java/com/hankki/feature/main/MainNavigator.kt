926512e6eb86de08e0a2c91a6c43eaa74b4f6677
0se0 <030ggong@gmail.com>
2024-07-17 03:14:43 +0900

    Mod #68: RollBack

--- feature/main/src/main/java/com/hankki/feature/main/MainNavigator.kt
+++ feature/main/src/main/java/com/hankki/feature/main/MainNavigator.kt
@@ -36,7 +36,7 @@ internal class MainNavigator(
         @Composable get() = navController
             .currentBackStackEntryAsState().value?.destination
 
-    val startDestination = UniversitySelection
+    val startDestination = Home
 
     val currentTab: MainTab?
         @Composable get() = MainTab.find { tab ->
