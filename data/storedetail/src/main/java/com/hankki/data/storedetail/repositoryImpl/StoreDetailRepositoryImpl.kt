a23f521630dea0a5f5957ba1a96a4ae6badabce0
0se0 <030ggong@gmail.com>
2024-07-16 12:08:07 +0900

    Feature #57: Create StoreDetailRepositoryImpl

---
+++ data/storedetail/src/main/java/com/hankki/data/storedetail/repositoryImpl/StoreDetailRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.hankki.data.storedetail.repositoryImpl
+
+import com.hankki.data.storedetail.datasource.StoreDetailDataSource
+import com.hankki.domain.storedetail.entity.StoreDetailResponseEntity
+import com.hankki.domain.storedetail.repository.StoreDetailRepository
+import javax.inject.Inject
+
+class StoreDetailRepositoryImpl @Inject constructor(
+    private val storeDetailDataSource: StoreDetailDataSource
+) : StoreDetailRepository {
+    override suspend fun getStoreDetail(
+        id: Long,
+    ): Result<StoreDetailResponseEntity> =
+        kotlin.runCatching {
+            storeDetailDataSource.getStoreDetail(
+                id,
+            ).data.toStoreDetailResponseEntity()
+        }
+}
