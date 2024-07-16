228778a90739bf821c17ba3782c53e4692abf1d2
0se0 <030ggong@gmail.com>
2024-07-17 03:12:30 +0900

    Feature #68: Create RepositoryModule

---
+++ data/universityselection/src/main/java/com/hankki/data/universityselection/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.hankki.data.universityselection.di
+
+import com.hankki.data.universityselection.repositoryImpl.UniversitySelectionRepositoryImpl
+import com.hankki.domain.universityselection.repository.UniversitySelectionRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindUniversitySelectionRepository(
+        universitySelectionRepositoryImpl: UniversitySelectionRepositoryImpl
+    ) : UniversitySelectionRepository
+}
