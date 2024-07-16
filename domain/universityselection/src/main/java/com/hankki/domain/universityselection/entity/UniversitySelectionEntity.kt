5a449dc16c7dff03859723989227777470a8d889
0se0 <030ggong@gmail.com>
2024-07-17 02:47:18 +0900

    Feature #68: Create UniversitySelectionEntity

---
+++ domain/universityselection/src/main/java/com/hankki/domain/universityselection/entity/UniversitySelectionEntity.kt
@@ -0,0 +1,8 @@
+package com.hankki.domain.universityselection.entity
+
+data class UniversitySelectionEntity (
+    val id: Int,
+    val name: String,
+    val longitude: Double,
+    val latitude: Double
+)
