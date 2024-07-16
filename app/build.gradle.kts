0df44ccbe3cb1a091e50586538ff41d8443de687
0se0 <030ggong@gmail.com>
2024-07-16 12:10:22 +0900

    Add #57: Add data.storedetail dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(projects.data.login)
     implementation(projects.data.reissuetoken)
     implementation(projects.data.home)
+    implementation(projects.data.storedetail)
 
     // core
     implementation(projects.core.network)
