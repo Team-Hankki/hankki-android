f31913a7d85d1d5969442608fe0c4150f0709218
0se0 <030ggong@gmail.com>
2024-07-17 03:12:20 +0900

    Feature #68: Create DataSourceModule

---
+++ data/universityselection/src/main/java/com/hankki/data/universityselection/di/DataSourceModule.kt
@@ -0,0 +1,19 @@
+package com.hankki.data.universityselection.di
+
+import com.hankki.data.universityselection.datasource.UniversitySelectionDataSource
+import com.hankki.data.universityselection.datasourceImpl.UniversirySelectionDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class DataSourceModule {
+    @Binds
+    @Singleton
+    abstract fun bindUniversitySelectionDataSource(
+        universirySelectionDataSourceImpl: UniversirySelectionDataSourceImpl
+    ): UniversitySelectionDataSource
+}
