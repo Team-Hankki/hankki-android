55c330201675968bdc68f06ef0064bf3a65c77b7
0se0 <030ggong@gmail.com>
2024-07-16 13:10:44 +0900

    Feature #57: api with temp id

--- feature/storedetail/src/main/java/com/hankki/feature/storedetail/StoreDetailViewModel.kt
+++ feature/storedetail/src/main/java/com/hankki/feature/storedetail/StoreDetailViewModel.kt
@@ -1,45 +1,48 @@
+package com.hankki.feature.storedetail
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hankki.core.common.utill.UiState
-import com.hankki.feature.storedetail.StoreState
-import com.hankki.feature.storedetail.model.MenuItem
-import com.hankki.feature.storedetail.model.StoreDetail
+import com.hankki.domain.storedetail.entity.StoreDetailResponseEntity
+import com.hankki.domain.storedetail.repository.StoreDetailRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class StoreDetailViewModel : ViewModel() {
-    private val _storeState = MutableStateFlow(StoreState())
-    val storeState: StateFlow<StoreState> get() = _storeState.asStateFlow()
+@HiltViewModel
+class StoreDetailViewModel @Inject constructor(
+    private val storeDetailRepository: StoreDetailRepository
+) : ViewModel() {
 
-    init {
-        val mockMenus = listOf(
-            MenuItem(name = "수육정식", price = 7900),
-            MenuItem(name = "제육정식", price = 8900),
-            MenuItem(name = "꼬막정식", price = 7900)
-        )
-        val mockStoreDetail = StoreDetail(
-            name = "한끼네 한정식",
-            category = "한식",
-            isLiked = false,
-            heartCount = 299,
-            imageUrls = listOf(),
-            menus = mockMenus
-        )
-
-        setStoreDetail(mockStoreDetail)
-        _storeState.value = _storeState.value.copy(
+    private val _storeState = MutableStateFlow(
+        StoreState(
             buttonLabels = persistentListOf(
                 "식당이 사라졌어요",
                 "더이상 8000원이하인 메뉴가 없어요",
                 "부적절한 제보에요"
             )
         )
+    )
+    val storeState: StateFlow<StoreState> get() = _storeState.asStateFlow()
+
+
+    fun fetchStoreDetail(id: Long = 3L) {
+        viewModelScope.launch {
+            _storeState.value = _storeState.value.copy(storeDetail = UiState.Loading)
+            val result = storeDetailRepository.getStoreDetail(id)
+            result.onSuccess {
+                setStoreDetail(it)
+            }.onFailure {
+                _storeState.value = _storeState.value.copy(storeDetail = UiState.Failure)
+            }
+        }
     }
 
-    private fun setStoreDetail(storeDetail: StoreDetail) {
+    private fun setStoreDetail(storeDetail: StoreDetailResponseEntity) {
         _storeState.value = _storeState.value.copy(
             storeDetail = UiState.Success(storeDetail),
             isLiked = storeDetail.isLiked,
