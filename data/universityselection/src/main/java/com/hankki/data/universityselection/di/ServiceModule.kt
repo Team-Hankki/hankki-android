e909c5b23f90b68f5f7fdd39f8a8a49c5e4e4aa7
0se0 <030ggong@gmail.com>
2024-07-17 03:12:40 +0900

    Feature #68: Create ServiceModule

---
+++ data/universityselection/src/main/java/com/hankki/data/universityselection/di/ServiceModule.kt
@@ -0,0 +1,20 @@
+package com.hankki.data.universityselection.di
+
+import com.hankki.core.network.JWT
+import com.hankki.data.universityselection.service.UniversitySelectionService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Provides
+    @Singleton
+    fun provideUniversitySelectionService(@JWT retrofit: Retrofit): UniversitySelectionService =
+        retrofit.create(UniversitySelectionService::class.java)
+}
