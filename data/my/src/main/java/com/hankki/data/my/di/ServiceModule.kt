527efbd478b621b4c5a2e1255917b66148247b6d
hyeumm <20221159@sungshin.ac.kr>
2024-07-18 01:45:47 +0900

    Chore #84: edit naming of module

--- data/my/src/main/java/com/hankki/data/my/di/ServiceModule.kt
+++ data/my/src/main/java/com/hankki/data/my/di/ServiceModule.kt
@@ -14,6 +14,6 @@ import javax.inject.Singleton
 object ServiceModule {
     @Provides
     @Singleton
-    fun providesMyDataSource(@JWT retrofit: Retrofit): MyService =
+    fun providesMyService(@JWT retrofit: Retrofit): MyService =
         retrofit.create(MyService::class.java)
 }
