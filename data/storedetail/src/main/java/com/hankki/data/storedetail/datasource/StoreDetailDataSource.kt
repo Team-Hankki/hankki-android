a75551c93575806ecfc6461b91f6d18e8283a757
0se0 <030ggong@gmail.com>
2024-07-17 13:46:29 +0900

    Feature #78: Update StoreDetailDataSource

--- data/storedetail/src/main/java/com/hankki/data/storedetail/datasource/StoreDetailDataSource.kt
+++ data/storedetail/src/main/java/com/hankki/data/storedetail/datasource/StoreDetailDataSource.kt
@@ -1,10 +1,19 @@
 package com.hankki.data.storedetail.datasource
 
 import com.hankki.core.network.BaseResponse
+import com.hankki.data.storedetail.response.StoreDetailHeartResponseDto
 import com.hankki.data.storedetail.response.StoreDetailResponseDto
 
 interface StoreDetailDataSource {
     suspend fun getStoreDetail(
         id:Long
     ): BaseResponse<StoreDetailResponseDto>
+
+    suspend fun postStoreDetailHearts(
+        id: Long
+    ): BaseResponse<StoreDetailHeartResponseDto>
+
+    suspend fun deleteStoreDetailHearts(
+        id: Long
+    ): BaseResponse<StoreDetailHeartResponseDto>
 }
