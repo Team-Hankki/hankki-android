c46809be36000a4f9c98777920da74fbff125bfb
hyeumm <20221159@sungshin.ac.kr>
2024-07-18 19:24:41 +0900

    Feature #97: MyService

--- data/my/src/main/java/com/hankki/data/my/service/MyService.kt
+++ data/my/src/main/java/com/hankki/data/my/service/MyService.kt
@@ -1,13 +1,21 @@
 package com.hankki.data.my.service
 
 import com.hankki.core.network.BaseResponse
+import com.hankki.core.network.CreatedBaseResponse
+import com.hankki.data.my.dto.request.NewJogboDto
 import com.hankki.data.my.dto.response.MyJogboDto
 import com.hankki.data.my.dto.response.UserInformationDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface MyService {
     @GET("/api/v1/users/me")
     suspend fun getUserInformation() : BaseResponse<UserInformationDto>
     @GET("/api/v1/users/me/favorites")
     suspend fun getMyJogboInformation() : BaseResponse<MyJogboDto>
+    @POST("/api/v1/favorites")
+    suspend fun postNewJogbo(
+        @Body body : NewJogboDto
+    ) : CreatedBaseResponse
 }
