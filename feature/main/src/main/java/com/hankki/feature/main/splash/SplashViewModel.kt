ed70187380492d5ff130e48f28798591dd9f7632
0se0 <030ggong@gmail.com>
2024-07-19 04:29:15 +0900

    Feature #111: token login, home

--- feature/main/src/main/java/com/hankki/feature/main/splash/SplashViewModel.kt
+++ feature/main/src/main/java/com/hankki/feature/main/splash/SplashViewModel.kt
@@ -2,6 +2,7 @@ package com.hankki.feature.main.splash
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hankki.core.datastore.TokenDataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -12,7 +13,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-
+    private val tokenDataStore: TokenDataStore
 ) : ViewModel() {
     private val _sideEffects = MutableSharedFlow<SplashSideEffect>()
     val sideEffects: SharedFlow<SplashSideEffect> get() = _sideEffects.asSharedFlow()
@@ -20,13 +21,11 @@ class SplashViewModel @Inject constructor(
     fun showSplash() {
         viewModelScope.launch {
             delay(SPLASH_DURATION)
-            // 로컬이랑 통신 -> 엑세스, 리프레시가 있는지. 진짜 있는지만.
-            // 검사해서 isEMpty -> boolean 이걸 변수에 담아.
-            val isLogined = true // 여기에 할당
 
-            if (isLogined) { // 토큰이 있다면 홈으로 이동
+            val isLogined = tokenDataStore.accessToken.isNotEmpty()
+            if (isLogined) {
                 _sideEffects.emit(SplashSideEffect.NavigateToHome)
-            } else { // 토큰이 없다면 로그인으로 이동
+            } else {
                 _sideEffects.emit(SplashSideEffect.NavigateLogin)
             }
         }
