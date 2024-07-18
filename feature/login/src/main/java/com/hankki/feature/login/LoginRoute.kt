4cbf2143756b432901bcc9672a0144eafdd8a634
0se0 <030ggong@gmail.com>
2024-07-19 00:02:13 +0900

    Feature #88: Create status bar, navigation bar transparent

--- feature/login/src/main/java/com/hankki/feature/login/LoginRoute.kt
+++ feature/login/src/main/java/com/hankki/feature/login/LoginRoute.kt
@@ -1,7 +1,5 @@
 package com.hankki.feature.login
 
-import android.app.Activity
-import androidx.activity.ComponentActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,22 +8,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.flowWithLifecycle
@@ -41,10 +34,6 @@ fun LoginRoute(
 ) {
     val viewModel: LoginViewModel = hiltViewModel()
     val lifecycleOwner = LocalLifecycleOwner.current
-    val context = LocalContext.current
-    val activity = context as ComponentActivity
-
-    WindowCompat.setDecorFitsSystemWindows(activity.window, false)
 
     LaunchedEffect(viewModel.loginSideEffects, lifecycleOwner) {
         viewModel.loginSideEffects
@@ -61,12 +50,12 @@ fun LoginRoute(
                 }
             }
     }
-
     SystemBarColorChanger(
         view = LocalView.current,
         color = Color.Transparent,
         shouldRollBack = false
     )
+
 
     LoginScreen(
         onLoginClick = { viewModel.startKakaoLogin() }
