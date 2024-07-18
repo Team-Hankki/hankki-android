17de0420201e40b51da49c3af706b0eebe3cdc11
hyeumm <20221159@sungshin.ac.kr>
2024-07-19 04:04:06 +0900

    Feature #108: MyRepository

--- domain/my/src/main/java/com/hankki/domain/my/repository/MyRepository.kt
+++ domain/my/src/main/java/com/hankki/domain/my/repository/MyRepository.kt
@@ -2,10 +2,13 @@ package com.hankki.domain.my.repository
 
 import com.hankki.domain.my.entity.request.NewJogboEntity
 import com.hankki.domain.my.entity.response.MyJogboEntity
+import com.hankki.domain.my.entity.response.StoreEntity
 import com.hankki.domain.my.entity.response.UserInformationEntity
 
 interface MyRepository {
     suspend fun getUserInformation() : Result<UserInformationEntity>
     suspend fun getMyJogboList() : Result<List<MyJogboEntity>>
     suspend fun createNewJogbo(body: NewJogboEntity): Result<Unit>
+    suspend fun getLikedStore() : Result<List<StoreEntity>>
+    suspend fun getReportedStore() : Result<List<StoreEntity>>
 }
