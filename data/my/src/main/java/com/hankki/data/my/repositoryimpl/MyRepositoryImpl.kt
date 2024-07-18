f6312e840665d1f8c68f852ffc8b30bfa9a0740a
hyeumm <20221159@sungshin.ac.kr>
2024-07-18 15:37:01 +0900

    Feature #95: MyRepositoryImpl

--- data/my/src/main/java/com/hankki/data/my/repositoryimpl/MyRepositoryImpl.kt
+++ data/my/src/main/java/com/hankki/data/my/repositoryimpl/MyRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package com.hankki.data.my.repositoryimpl
 
 import com.hankki.data.my.datasource.MyDataSource
+import com.hankki.data.my.dto.response.toEntity
 import com.hankki.domain.my.entity.MyRepository
+import com.hankki.domain.my.entity.response.MyJogboEntity
 import javax.inject.Inject
 
 class MyRepositoryImpl @Inject constructor(
@@ -9,5 +11,8 @@ class MyRepositoryImpl @Inject constructor(
 ) : MyRepository {
     override suspend fun getUserInformation() = runCatching {
         myDataSource.getUserInformation().data.toEntity()
+    }
+    override suspend fun getMyJogboList() :Result<List<MyJogboEntity>> = runCatching {
+        myDataSource.getMyJogboList().data.toEntity()
     }
 }
