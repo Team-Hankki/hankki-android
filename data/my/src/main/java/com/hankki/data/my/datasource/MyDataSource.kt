a0e51898a01c00dcd63ac495da56a283d73a18f5
hyeumm <20221159@sungshin.ac.kr>
2024-07-18 19:23:42 +0900

    Feature #97: MyDataSource

--- data/my/src/main/java/com/hankki/data/my/datasource/MyDataSource.kt
+++ data/my/src/main/java/com/hankki/data/my/datasource/MyDataSource.kt
@@ -1,11 +1,13 @@
 package com.hankki.data.my.datasource
 
 import com.hankki.core.network.BaseResponse
+import com.hankki.core.network.CreatedBaseResponse
+import com.hankki.data.my.dto.request.NewJogboDto
 import com.hankki.data.my.dto.response.MyJogboDto
 import com.hankki.data.my.dto.response.UserInformationDto
 
 interface MyDataSource {
     suspend fun getUserInformation(): BaseResponse<UserInformationDto>
     suspend fun getMyJogboList(): BaseResponse<MyJogboDto>
-
+    suspend fun createNewJogbo(body: NewJogboDto): CreatedBaseResponse
 }
