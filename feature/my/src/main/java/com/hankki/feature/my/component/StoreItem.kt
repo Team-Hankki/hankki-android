1c16e96cd70e97b94fc7c80c843f7693fc2345e9
hyeumm <20221159@sungshin.ac.kr>
2024-08-05 19:01:55 +0900

    Mod #144: add padding to item

--- feature/my/src/main/java/com/hankki/feature/my/component/StoreItem.kt
+++ feature/my/src/main/java/com/hankki/feature/my/component/StoreItem.kt
@@ -55,8 +55,7 @@ fun StoreItem(
             .wrapContentHeight()
             .clip(RoundedCornerShape(10.dp))
             .background(White)
-            .padding(vertical = 16.dp),
-            //.noRippleClickable(onClickItem),
+            .padding(vertical = 16.dp, horizontal = 22.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         AsyncImage(
