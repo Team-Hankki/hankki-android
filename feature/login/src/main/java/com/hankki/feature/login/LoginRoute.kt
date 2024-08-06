9fc88df84fc3d8c27f89d990472835432be098c4
0se0 <030ggong@gmail.com>
2024-08-06 22:08:39 +0900

    Delete #152: unused import

--- feature/login/src/main/java/com/hankki/feature/login/LoginRoute.kt
+++ feature/login/src/main/java/com/hankki/feature/login/LoginRoute.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
