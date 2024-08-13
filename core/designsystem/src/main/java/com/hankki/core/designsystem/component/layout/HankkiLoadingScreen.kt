15a3c66d09d91c1c03bda7f5df1b0d7f6cd3bcb2
Dongmin <pdm001125@naver.com>
2024-08-14 03:14:12 +0900

    Feature #188: change loading lottie

--- core/designsystem/src/main/java/com/hankki/core/designsystem/component/layout/HankkiLoadingScreen.kt
+++ core/designsystem/src/main/java/com/hankki/core/designsystem/component/layout/HankkiLoadingScreen.kt
@@ -3,6 +3,7 @@ package com.hankki.core.designsystem.component.layout
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
@@ -11,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -49,11 +51,7 @@ fun HankkiLoadingScreen(modifier: Modifier = Modifier) {
         LottieAnimation(
             composition = composition,
             iterations = LottieConstants.IterateForever,
-            modifier = Modifier
-                .statusBarsPadding()
-                .fillMaxSize()
-                .align(Alignment.BottomCenter),
-            contentScale = ContentScale.FillWidth
+            modifier = Modifier.size(68.dp),
         )
     }
 }
