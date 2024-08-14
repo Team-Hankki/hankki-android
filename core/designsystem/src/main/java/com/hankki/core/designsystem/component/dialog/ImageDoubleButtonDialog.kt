c63f97bc03e5f7fcbe965111ce55665d39ce32a3
Dongmin <pdm001125@naver.com>
2024-08-15 02:53:37 +0900

    Fix #195: set imageDialog padding

--- core/designsystem/src/main/java/com/hankki/core/designsystem/component/dialog/ImageDoubleButtonDialog.kt
+++ core/designsystem/src/main/java/com/hankki/core/designsystem/component/dialog/ImageDoubleButtonDialog.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -21,6 +25,7 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
 import androidx.compose.ui.window.Dialog
 import com.hankki.core.designsystem.R
 import com.hankki.core.designsystem.component.button.HankkiButton
@@ -43,13 +48,14 @@ fun ImageDoubleButtonDialog(
 ) {
     Dialog(onDismissRequest = onNegativeButtonClicked) {
         Card(
+            modifier = Modifier.widthIn(max = 316.dp),
             shape = RoundedCornerShape(24.dp),
             colors = CardDefaults.cardColors(
                 containerColor = White
             ),
         ) {
             Column(
-                modifier = Modifier.padding(vertical = 24.dp, horizontal = 22.dp),
+                modifier = Modifier.fillMaxWidth().padding(vertical = 24.dp, horizontal = 22.dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
