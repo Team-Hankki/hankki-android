d0cd0ba4fdc937d4e182e2105e7051e04a50a8e9
hyeumm <20221159@sungshin.ac.kr>
2024-08-17 23:48:32 +0900

    Mod #193 : delete padding value

--- feature/my/src/main/java/com/hankki/feature/my/mystore/MyStoreRoute.kt
+++ feature/my/src/main/java/com/hankki/feature/my/mystore/MyStoreRoute.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.HorizontalDivider
@@ -91,7 +92,7 @@ fun MyStoreScreen(
 ) {
     Column(
         modifier = modifier
-            .padding((paddingValues))
+            .statusBarsPadding()
             .background(White)
             .fillMaxSize()
     ) {
