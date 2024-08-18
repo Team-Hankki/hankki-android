aa80f8032d4bbd50f8f8756edf146cc66db4522e
0se0 <030ggong@gmail.com>
2024-08-18 19:49:05 +0900

    Fix #201: Update bottom padding

--- feature/storedetail/src/main/java/com/hankki/feature/storedetail/StoreDetailRoute.kt
+++ feature/storedetail/src/main/java/com/hankki/feature/storedetail/StoreDetailRoute.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -399,6 +400,8 @@ fun StoreDetailScreen(
                     textStyle = HankkiTheme.typography.sub3,
                     enabled = selectedIndex != -1
                 )
+
+                Spacer(modifier = Modifier.height(54.dp))
             }
         }
     }
