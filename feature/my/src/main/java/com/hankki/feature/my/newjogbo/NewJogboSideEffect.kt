85900b1a0bec83769168a2bdf7c50584edd05933
hyeumm <20221159@sungshin.ac.kr>
2024-08-21 02:18:01 +0900

    Mod #207 : add showerrordialog sideeffect

--- feature/my/src/main/java/com/hankki/feature/my/newjogbo/NewJogboSideEffect.kt
+++ feature/my/src/main/java/com/hankki/feature/my/newjogbo/NewJogboSideEffect.kt
@@ -2,4 +2,5 @@ package com.hankki.feature.my.newjogbo
 
 sealed class NewJogboSideEffect {
  data object NavigateToNewJogbo : NewJogboSideEffect()
+ data object ShowErrorDialog : NewJogboSideEffect()
 }
