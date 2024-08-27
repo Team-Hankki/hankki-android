efa7dbaf2cbbda31d252c56eb4e75ef1b631529c
0se0 <030ggong@gmail.com>
2024-08-28 01:14:40 +0900

    Feature #225: Update loading sideEffect

--- feature/my/src/main/java/com/hankki/feature/my/mypage/MySideEffect.kt
+++ feature/my/src/main/java/com/hankki/feature/my/mypage/MySideEffect.kt
@@ -4,4 +4,6 @@ sealed class MySideEffect {
     data object ShowLogoutSuccess : MySideEffect()
     data object ShowDeleteWithdrawSuccess : MySideEffect()
     data class ShowWebView(val type: String) : MySideEffect()
+    data object ShowLoading : MySideEffect()
+    data object HideLoading : MySideEffect()
 }
