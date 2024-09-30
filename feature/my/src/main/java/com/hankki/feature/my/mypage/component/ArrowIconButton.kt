4e5a7768a2db0e7c90d7b42a8739fa1f4c4ded0e
hyeumm <20221159@sungshin.ac.kr>
2024-09-30 10:46:01 +0900

    Chore #245 : update component

--- feature/my/src/main/java/com/hankki/feature/my/mypage/component/ArrowIconButton.kt
+++ feature/my/src/main/java/com/hankki/feature/my/mypage/component/ArrowIconButton.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,10 +13,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hankki.core.common.extension.noRippleClickable
-import com.hankki.core.designsystem.theme.Gray200
-import com.hankki.core.designsystem.theme.Gray900
+import com.hankki.core.designsystem.theme.Gray600
 import com.hankki.core.designsystem.theme.HankkiTheme
 import com.hankki.core.designsystem.theme.HankkijogboTheme
+import com.hankki.feature.my.R
 
 @Composable
 fun ArrowIconButton(
@@ -35,15 +34,14 @@ fun ArrowIconButton(
     ) {
         Text(
             text = itemTitle,
-            style = HankkiTheme.typography.body5,
-            color = Gray900
+            style = HankkiTheme.typography.body6,
+            color = Gray600
         )
         Image(
-            painterResource(id = com.hankki.core.designsystem.R.drawable.ic_arrow_right),
+            painterResource(id = R.drawable.ic_arrow_right),
             contentDescription = "ic_arrow_right"
         )
     }
-    HorizontalDivider(thickness = 1.dp, color = Gray200)
 }
 
 @Composable
