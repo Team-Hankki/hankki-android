04bc518cd4966257858b8b269dd8467b42b94fe4
Dongmin <pdm001125@naver.com>
2024-10-05 12:59:15 +0900

    Feature #248: add university icon

--- feature/home/src/main/java/com/hankki/feature/home/HomeRoute.kt
+++ feature/home/src/main/java/com/hankki/feature/home/HomeRoute.kt
@@ -13,6 +13,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -27,6 +28,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -53,10 +55,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -88,6 +92,7 @@ import com.hankki.feature.home.MapConstants.DEFAULT_ZOOM
 import com.hankki.feature.home.R.drawable.ic_coin
 import com.hankki.feature.home.R.drawable.ic_empty_bowl
 import com.hankki.feature.home.R.drawable.ic_marker
+import com.hankki.feature.home.R.drawable.ic_university
 import com.hankki.feature.home.R.drawable.ic_sort
 import com.hankki.feature.home.component.DropdownFilterChip
 import com.hankki.feature.home.component.RepositionButton
@@ -392,14 +397,19 @@ fun HomeScreen(
                     modifier = Modifier.noRippleClickable(navigateToUniversitySelection),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Spacer(modifier = Modifier.padding(start = 22.dp))
+                    Spacer(modifier = Modifier.padding(start = 16.dp))
+                    Image(
+                        imageVector = ImageVector.vectorResource(id = ic_university),
+                        contentDescription = "university",
+                        modifier = Modifier.size(18.dp).padding(horizontal = 2.dp)
+                    )
                     Text(
                         text = universityName,
                         style = HankkiTheme.typography.suitSub1,
                         color = Gray900
                     )
                     Icon(
-                        painter = painterResource(id = R.drawable.ic_dropdown_btn),
+                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_dropdown_btn),
                         contentDescription = "button",
                         tint = Gray300
                     )
