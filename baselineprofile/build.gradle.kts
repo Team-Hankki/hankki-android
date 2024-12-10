ee9d2ae0635aea179f9af092f05686cd6782aca8
Dongmin <pdm001125@naver.com>
2024-12-10 23:17:39 +0900

    fix : baselineProfile version

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -56,7 +56,7 @@ androidComponents {
         val artifactsLoader = v.artifacts.getBuiltArtifactsLoader()
         v.instrumentationRunnerArguments.put(
             "targetAppId",
-            v.testedApks.map { artifactsLoader.load(it)?.applicationId }
+            v.testedApks.map { artifactsLoader.load(it)?.applicationId ?: "" }
         )
     }
 }
