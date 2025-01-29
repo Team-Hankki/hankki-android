b250f5bf2db92232ce9a391a3480cf9ec7726d5b
Dongmin <pdm001125@naver.com>
2025-01-29 21:37:53 +0900

    feature/#294: fix animateScrollAroundItem function

--- core/common/src/main/java/com/hankki/core/common/extension/Modifier.kt
+++ core/common/src/main/java/com/hankki/core/common/extension/Modifier.kt
@@ -7,13 +7,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
@@ -30,13 +26,10 @@ import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.findRootCoordinates
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
-import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 inline fun Modifier.noRippleClickable(
@@ -137,15 +130,18 @@ fun Modifier.ignoreNextModifiers(): Modifier {
 
 fun Modifier.animateScrollAroundItem(
     scrollState: ScrollState,
-    yPosition: Float = 50f,
 ): Modifier = composed {
     var scrollToPosition by remember {
         mutableFloatStateOf(0f)
     }
     val coroutineScope = rememberCoroutineScope()
+
     this
         .onGloballyPositioned { coordinates ->
-            scrollToPosition = coordinates.positionInParent().y + yPosition
+            val itemTop = coordinates.positionInParent().y
+            val itemHeight = coordinates.size.height
+
+            scrollToPosition = itemTop + itemHeight
         }
         .onFocusEvent {
             if (it.isFocused) {
@@ -154,17 +150,4 @@ fun Modifier.animateScrollAroundItem(
                 }
             }
         }
-}
-
-fun Modifier.advancedImePadding() = composed {
-    var consumePadding by remember { mutableIntStateOf(0) }
-
-    onGloballyPositioned { coordinates ->
-        val rootCoordinate = coordinates.findRootCoordinates()
-        val bottom = coordinates.positionInWindow().y + coordinates.size.height
-
-        consumePadding = (rootCoordinate.size.height - bottom).toInt()
-    }
-        .consumeWindowInsets(PaddingValues(bottom = (consumePadding / LocalDensity.current.density).dp))
-        .imePadding()
 }
